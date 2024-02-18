f9fd9c4d50277e44423be6805c51ee9898dbe9b7
unknown <kmnj2100@gmail.com>
2024-02-18 13:57:23 +0900

    discord bot exam

---
+++ apps/index.js
@@ -0,0 +1,34 @@
+const { Client, GatewayIntentBits, Collection } = require("discord.js");
+const { Guilds, GuildMessages, MessageContent } = GatewayIntentBits;
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const client = new Client({
+  intents: [Guilds, GuildMessages, MessageContent],
+});
+const {
+  generateDependencyReport,
+  joinVoiceChannel,
+  getVoiceConnection,
+} = require("@discordjs/voice");
+
+client.login(
+  "MTIwNDA1MTM5OTA1MTMyMTQyNA.GGMYD5.iATIpMmnYdOfG_i-4KlN8qviW3TPKQjaoFT4XY"
+);
+
+client.once("ready", () => console.log(client.user.tag + "start!"));
+
+client.on("messageCreate", (message) => {
+  if (message.content === "$join") {
+    const connection = joinVoiceChannel({
+      channelId: message.member.voice.channelId,
+      guildId: message.guild.id,
+      adapterCreator: message.guild.voiceAdapterCreator,
+    });
+  } else if (message.content === "$out") {
+    const connection = joinVoiceChannel({
+      channelId: message.member.voice.channelId,
+      guildId: message.guild.id,
+      adapterCreator: message.guild.voiceAdapterCreator,
+    }).destroy();
+  }
+});
